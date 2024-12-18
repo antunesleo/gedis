fea6398e4a62d034771226b807c2877dd2b1d400
Leo Antunes <antunesleo4@gmail.com>
2024-12-18 10:05:18 -0300

    refactor

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -207,20 +208,28 @@ func serializerInteger64(intToSerialize int64) []byte {
     return []byte(fmt.Sprintf(":%d\r\n", intToSerialize))
 }
 
-func cmdPing() []byte {
+type Command interface {
+    execute(message[][]byte) []byte
+}
+
+type CommandPing struct {}
+func (command CommandPing) execute(message[][]byte) []byte {
     return serializeSimpleStringFromString("PONG")
 }
 
-func cmdEcho(message[][]byte) []byte {
+type CommandEcho struct {}
+func (command CommandEcho) execute(message[][]byte) []byte {
     return serializeSimpleStringFromByteArray(message[1])
 }
 
-func cmdSet(message[][]byte) []byte {
+type CommandSet struct {}
+func (command CommandSet) execute(message[][]byte) []byte {
     cache[string(message[1])] = message[2]
     return serializeSimpleStringFromString("OK")
 }
 
-func cmdGet(message[][]byte) []byte {
+type CommandGet struct {}
+func (command CommandGet) execute(message[][]byte) []byte {
     value, ok := cache[string(message[1])]
     if ok {
         return serializeSimpleStringFromByteArray(value)
@@ -228,7 +237,8 @@ func cmdGet(message[][]byte) []byte {
     return serializeError("doesn't exist")
 }
 
-func cmdExists(message[][]byte) []byte {
+type CommandExists struct {}
+func (command CommandExists) execute(message[][]byte) []byte {
     var existsCount = 0
     var itemIndex = 1
     for itemIndex < len(message) {
@@ -242,23 +252,23 @@ func cmdExists(message[][]byte) []byte {
     return serializerInteger(existsCount)
 }
 
-func cmdDel(message[][]byte) []byte {
+type CommandDel struct {}
+func (command CommandDel) execute(message[][]byte) []byte {
     var existsCount = 0
     var itemIndex = 1
     for itemIndex < len(message) {
-        var key = string(message[itemIndex])
-        _, ok := cache[key]
+        _, ok := cache[string(message[itemIndex])]
         if ok {
-            delete(cache, key)
             existsCount += 1
         }
         itemIndex += 1
     }
 
-    return serializerInteger(existsCount)   
+    return serializerInteger(existsCount)
 }
 
-func cmdIncr(message[][]byte) []byte {
+type CommandIncr struct {}
+func (command CommandIncr) execute(message[][]byte) []byte {
     var key = string(message[1])
     value, ok := cache[key]
 
@@ -278,6 +288,26 @@ func cmdIncr(message[][]byte) []byte {
     return serializerInteger64(newValue)
 }
 
+func getCommand(message[][]byte) (Command, error) {
+    if bytes.Equal(message[0], SET_BYTE_ARRAY) {
+        return CommandSet{}, nil
+    } else if bytes.Equal(message[0], GET_BYTE_ARRAY) {
+        return CommandGet{}, nil
+    } else if bytes.Equal(message[0], PING_BYTE_ARRAY) {
+        return CommandPing{}, nil
+    } else if bytes.Equal(message[0], ECHO_BYTE_ARRAY) {
+        return CommandEcho{}, nil
+    } else if bytes.Equal(message[0], EXISTS_BYTE_ARRAY) {
+        return CommandExists{}, nil
+    } else if bytes.Equal(message[0], DEL_BYTE_ARRAY) {
+        return CommandDel{}, nil
+    } else if bytes.Equal(message[0], INCR_BYTE_ARRAY) {
+        return CommandIncr{}, nil
+    } else {
+        return nil, errors.New("no command found for message")
+    }
+}
+
 func periodicallySaveSnapshot() {
     for {
         time.Sleep(5 * time.Second)
@@ -286,21 +316,21 @@ func periodicallySaveSnapshot() {
 }
 
 func main() {
-    err, newCache := restoreSnapshot()
-    if err == nil {
+    restoreErr, newCache := restoreSnapshot()
+    if restoreErr == nil {
         cache = newCache
     }
 
-    listerner, err := net.Listen("tcp", "localhost:6379")
-    if err != nil {
-        fmt.Println(err)
+    listerner, listenErr := net.Listen("tcp", "localhost:6379")
+    if listenErr != nil {
+        fmt.Println(listenErr)
         return
-        // handle error
     }
     for {
-        conn, err := listerner.Accept()
-        if err != nil {
-            // handle error
+        conn, acceptErr := listerner.Accept()
+        if acceptErr != nil {
+            fmt.Println(acceptErr)
+            continue
         }
         go handleConnection(conn)
         go periodicallySaveSnapshot()
@@ -312,38 +342,30 @@ func handleConnection(conn net.Conn) {
 
     buffer := make([]byte, 8192)
     for {
-        n, err := conn.Read(buffer)
-        if err != nil {
-            fmt.Println("Error:", err)
+        bytesNumber, connReadErr := conn.Read(buffer)
+        if connReadErr != nil {
+            fmt.Println("Error:", connReadErr)
             return
         }
 
-        result := []byte{}
-        message := deserialize(buffer[:n])
+        message := deserialize(buffer[:bytesNumber])
+        command, getCommandErr := getCommand(message)
 
-        if bytes.Equal(message[0], SET_BYTE_ARRAY) {
-            result = cmdSet(message)
-        } else if bytes.Equal(message[0], GET_BYTE_ARRAY) {
-            result = cmdGet(message)
-        } else if bytes.Equal(message[0], PING_BYTE_ARRAY) {
-            result = cmdPing()
-        } else if bytes.Equal(message[0], ECHO_BYTE_ARRAY) {
-            result = cmdEcho(message)
-        } else if bytes.Equal(message[0], EXISTS_BYTE_ARRAY) {
-            result = cmdExists(message)
-        } else if bytes.Equal(message[0], DEL_BYTE_ARRAY) {
-            result = cmdDel(message)
-        } else if bytes.Equal(message[0], INCR_BYTE_ARRAY) {
-            result = cmdIncr(message)
-        }
-        
-        if len(result) == 0 {
+        var result []byte
+        if getCommandErr != nil {
             result = serializeError("not implemented")
+        } else {
+            result = command.execute(message)
         }
 
-        _, err = conn.Write(result)
-        if err != nil {
-            fmt.Println("Error:", err)
+        totalWritten := 0
+        for totalWritten < len(result) {
+            bytesWrittenNumbers, connWriteErr := conn.Write(result[totalWritten:])
+            if connWriteErr != nil {
+                fmt.Println("Error:", connWriteErr)
+                return
+            }
+            totalWritten += bytesWrittenNumbers
         }
     }
 }
